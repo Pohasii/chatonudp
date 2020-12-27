e271ae3551adacb6db08e831ed8e1b546e56b19f
Ficus <pogasiid@gmail.com>
2020-12-27 18:57:17 +0100

    create echoServer and console client

--- Client/main.go
+++ Client/main.go
@@ -1,8 +1,61 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"net"
+	"os"
+	"strings"
 )
+
 func main() {
-	fmt.Println("Start Client")
+
+	sAddr := "192.168.0.52:12345"
+	serverAddr, err := net.ResolveUDPAddr("udp", sAddr)
+	listener, err := net.DialUDP("udp", nil, serverAddr)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("The UDP server is %serverAddr\n", listener.RemoteAddr().String())
+	defer listener.Close()
+
+	stop := true
+
+	go reader(listener)
+
+	for stop {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print(">> ")
+		text, _ := reader.ReadString('\n')
+
+		text = strings.TrimSuffix(text, "\n")
+		data := []byte(text)
+
+		_, err = listener.Write(data)
+
+		if strings.TrimSpace(string(data)) == "STOP" {
+			fmt.Println("off UDP client!")
+			return
+		}
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+}
+
+func reader(listener *net.UDPConn) {
+
+	buffer := make([]byte, 1024)
+	for {
+		n, _, err := listener.ReadFromUDP(buffer)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("Reply: %s\n", string(buffer[0:n]))
+	}
 }
--- main.go
+++ main.go
@@ -2,8 +2,42 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
 )
 
 func main() {
-	fmt.Println("Start server")
+	fmt.Println("Server Start")
+
+	// init server
+	sAddr := "192.168.0.52:12345" //localhost:0
+	adr, err := net.ResolveUDPAddr("udp", sAddr) //192.168.0.52:12345
+	if err != nil {
+		log.Println(err)
+	}
+
+	Conn, err := net.ListenUDP("udp", adr)
+	if err != nil {
+		log.Println(err)
+	}
+
+	echoServer(Conn)
+}
+
+func echoServer(Conn *net.UDPConn) {
+	defer Conn.Close()
+
+	var sms = make([]byte, 512)
+
+	for {
+		size, caddr, err := Conn.ReadFromUDP(sms)
+		if err != nil {
+			log.Println(err)
+		}
+
+		if size > 0 {
+			Conn.WriteTo(sms, caddr)
+			fmt.Println("caddr: ", caddr, "size: ", size, "mess: ", string(sms[:size]))
+		}
+	}
 }
